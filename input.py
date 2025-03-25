2e89c774b9ef8b7317cdd0b75eb371c0e60a04ee
玛奇娜 <98892565+deckerwac@users.noreply.github.com>
2025-03-25 20:48:38 +0700

    Update input.py

--- input.py
+++ input.py
@@ -7,15 +7,15 @@ def collect_data():
 
     print("\n>.<\n")
 
-    person["name"] = input("$ Name: ")
-    person["dob"] = input("$ DOB: ")
-    person["address"] = input("$ Address: ")
-    person["email"] = input("$ Email: ")
+    person["name"] = input("[+] Name: ")
+    person["dob"] = input("[+] DOB: ")
+    person["address"] = input("[+] Address: ")
+    person["email"] = input("[+] Email: ")
 
     return person
 
 
-def load_existing_data(filename='/result/'):  # 设置保存位置
+def load_existing_data(filename='/result/result.json'):  # 设置保存位置
     if os.path.exists(filename):
         with open(filename, 'r') as json_file:
             data = json.load(json_file)
@@ -23,7 +23,7 @@ def load_existing_data(filename='/result/'):  # 设置保存位置
     return []
 
 
-def save_data_to_json(data, filename='/result/'):  # 设置保存位置
+def save_data_to_json(data, filename='/result/result.json'):  # 设置保存位置
     with open(filename, 'w') as json_file:
         json.dump(data, json_file, indent=1)
     print(f"\n➜ 文件夹位置 : {filename}\n")
